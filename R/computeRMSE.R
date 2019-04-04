4cc57833536542cde719c24319ec5eede16b4c53
jhorzek <orzekjan@hu-berlin.de>
2019-04-04 20:14:50 +0200

    RMSE update

--- R/computeRMSE.R
+++ R/computeRMSE.R
@@ -1,8 +1,16 @@
+#' compute the Root Mean Squared Error for parameters in A and S
+#'
+#' @author Jannik Orzek
+#' @import OpenMx
+#'
+#' @examples
+#'
+#' @export
 computeRMSE <- function(A_est,S_est, Apop, Spop, Afree, Sfree){
   RMSE_Amat <- sqrt(mean((Apop[Afree] - A_est[Afree])^2))
   RMSE_Smat <- sqrt(mean((Spop[Sfree] - S_est[Sfree])^2))
   RMSE_comb <- sqrt(mean(c((Apop[Afree] - A_est[Afree])^2,(Spop[Sfree] - S_est[Sfree])^2)))
-  
+
   ret = matrix(NA, nrow= 3, ncol = 1)
   rownames(ret) <- c("RMSE_Amat", "RMSE_Smat", "RMSE_comb")
   ret["RMSE_Amat",1] <- RMSE_Amat
