31840978af9cb0fc995d402f6ab37886e214eac0
jhorzek <orzekjan@hu-berlin.de>
2019-04-25 11:47:01 +0200

    changed diffusion

--- R/getQdeltaT.R
+++ R/getQdeltaT.R
@@ -1,6 +1,18 @@
+#' getQdeltaT
+#'
+#'
+#' @author Jannik Orzek
+#' @import OpenMx
+#'
+#' @examples
+#'
+#' @export
+#'
+#'
+
 getQdeltaT <- function(DRIFT, DIFFUSION, deltaT){
   Qmat <- DIFFUSION%*%t(DIFFUSION)
-  Asharp <- kronecker(DRIFT,diag(ncol(DRIFT))) + kronecker(diag(ncol(DRIFT)), DRIFT) 
+  Asharp <- kronecker(DRIFT,diag(ncol(DRIFT))) + kronecker(diag(ncol(DRIFT)), DRIFT)
   QdeltaT <- matrix(solve(Asharp) %*% (expm(Asharp*deltaT)-diag(ncol(expm(Asharp*deltaT)))) %*% cvectorize(Qmat), ncol= ncol(DRIFT), nrow = nrow(DRIFT), byrow = T)
   return(QdeltaT)
   }
